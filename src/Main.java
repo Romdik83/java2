7f49a823661257e1cfff2c930adc83ff6570bbe1
Romdik <romdik83@gmail.com>
2025-04-22 15:44:43 +0300

    calculating bonuses

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       int price = 13676;
+       int amount = 20;
+       int theMile = 1;
+
+       int bonus = price / amount * theMile;
+
+       System.out.println("Итоговый бонус: " + bonus);
     }
 }
